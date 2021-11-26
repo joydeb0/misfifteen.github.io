3e3157d32fbcf2a4126a4ef6f3732366f0e92b73
joydeb0 <82994303+joydeb0@users.noreply.github.com>
2021-11-26 15:20:48 +0600

    Add files via upload

---
+++ studentsdata.js
@@ -0,0 +1,31 @@
+var UserInfo = [
+    {
+        name: "Joy Barman",
+        avater: "avater.jpg",
+        roll: "59",
+        reg: "500",
+        email: "joy@gmail.com",
+        hometown: "netrakona",
+        blood: "A+",
+        SCODE: "1010",
+        phone: "01924251734"
+    },{
+        name: "Deb",
+        avater: "avater1.jpg",
+        roll: "81",
+        reg: "800",
+        email: "deb@gmail.com",
+        hometown: "dhaka",
+        blood: "O+",
+        SCODE: "2020"
+    },{
+        name: "jeb",
+        avater: "avater1.jpg",
+        roll: "55",
+        reg: "800",
+        email: "deb@gmail.com",
+        hometown: "dhaka",
+        blood: "O+",
+        SCODE: "2020"
+    }
+]
